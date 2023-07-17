18ef87be37b6dd1f12397d682547530a00508bb8
ssangjun <105031887+ssangjun@users.noreply.github.com>
2023-07-17 20:45:09 +0900

    Chore: 스웨거 설정 (#2)
    * Chore: 스웨거 설정
    
    * Style: 시큐리티 기능 제거 코드 주석화

--- src/main/java/com/example/travelhana/TravelhanaApplication.java
+++ src/main/java/com/example/travelhana/TravelhanaApplication.java
@@ -2,7 +2,9 @@ package com.example.travelhana;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
+//@SpringBootApplication(exclude = SecurityAutoConfiguration.class) //스프링 시큐리티 기능 제거
 @SpringBootApplication
 public class TravelhanaApplication {
 
