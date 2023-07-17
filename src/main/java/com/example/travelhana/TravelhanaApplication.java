60ad5285fcbf3595c1aef7c00f20070a2e190959
ssangjun <ssangjun4dev@gmail.com>
2023-07-17 17:35:26 +0900

    Chore: 스웨거 설정

--- src/main/java/com/example/travelhana/TravelhanaApplication.java
+++ src/main/java/com/example/travelhana/TravelhanaApplication.java
@@ -2,8 +2,9 @@ package com.example.travelhana;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class) //스프링 시큐리티 기능 제거
 public class TravelhanaApplication {
 
     public static void main(String[] args) {
