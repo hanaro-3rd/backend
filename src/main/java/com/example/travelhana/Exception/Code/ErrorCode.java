c31d8930faba6a0a669fa3257ca7e4ef90421cb1
PARKYUNJU <pyunj6767@gmail.com>
2023-07-28 17:45:11 +0900

    Refactor: ErrorCode의 status를 상태코드에서 enum 이름으로 변경

--- src/main/java/com/example/travelhana/Exception/Code/ErrorCode.java
+++ src/main/java/com/example/travelhana/Exception/Code/ErrorCode.java
@@ -12,13 +12,13 @@ import javax.annotation.security.DenyAll;
 public enum ErrorCode {
 
     //비밀번호 형식 에러
-    INVALID_PASSWORD(400, "400", "비밀번호는 6개의 숫자로 구성해주세요"),
+    INVALID_PASSWORD(400, "INVALID_PASSWORD", "비밀번호는 6개의 숫자로 구성해주세요"),
     //환전 시 원화 잔액 부족 에러
-    INSUFFICIENT_BALANCE(500, "500", "계좌 잔액이 부족합니다."),
+    INSUFFICIENT_BALANCE(500, "INSUFFICIENT_BALANCE", "계좌 잔액이 부족합니다."),
     //환전 시 화폐단위 입력 에러
-    INVALID_EXCHANGEUNIT(500, "500", "유효하지 않은 화폐단위입니다."),
+    INVALID_EXCHANGEUNIT(500, "INVALID_EXCHANGEUNIT", "유효하지 않은 화폐단위입니다."),
     //존재하지 않는 계좌 에러
-    NO_ACCOUNT(500, "500", "계좌가 존재하지 않습니다.");
+    NO_ACCOUNT(500, "NO_ACCOUNT", "계좌가 존재하지 않습니다.");
 
     private int statusCode;
     private String status;
--- src/main/java/com/example/travelhana/Exception/Code/SuccessCode.java
+++ src/main/java/com/example/travelhana/Exception/Code/SuccessCode.java
@@ -10,15 +10,15 @@ import lombok.NoArgsConstructor;
 public enum SuccessCode {
 
     // 조회 성공 코드 (HTTP Response: 200 OK)
-    SELECT_SUCCESS(200, "200", "SELECT SUCCESS"),
+    SELECT_SUCCESS(200, "SELECT_SUCCESS", "조회 성공"),
     // 삭제 성공 코드 (HTTP Response: 200 OK)
-    DELETE_SUCCESS(200, "200", "DELETE SUCCESS"),
+    DELETE_SUCCESS(200, "DELETE_SUCCESS", "삭제 성공"),
     // 삽입 성공 코드 (HTTP Response: 201 Created)
-    INSERT_SUCCESS(201, "201", "INSERT SUCCESS"),
+    INSERT_SUCCESS(201, "INSERT_SUCCESS", "삽입 성공"),
     // 수정 성공 코드 (HTTP Response: 201 Created)
-    UPDATE_SUCCESS(204, "204", "UPDATE SUCCESS"),
+    UPDATE_SUCCESS(204, "UPDATE_SUCCESS", "수정 성공"),
     // 인증 성공 코드 (HTTP Response: 200 OK)
-    AUTH_SUCCESS(200, "200", "SIGNIN SUCCESS");
+    AUTH_SUCCESS(200, "AUTH_SUCCESS", "인증 성공");
 
     private int statusCode;
     private String status;
