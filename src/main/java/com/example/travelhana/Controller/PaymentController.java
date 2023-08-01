f4563356f9c284d9aeb566047d55dfda79a7f0b9
eternalclash <suchasucha@naver.com>
2023-08-01 09:01:08 +0900

    Refactor: 수정 기능 수정

--- src/main/java/com/example/travelhana/Controller/PaymentController.java
+++ src/main/java/com/example/travelhana/Controller/PaymentController.java
@@ -1,4 +1,11 @@
 package com.example.travelhana.Controller;
 
+import com.example.travelhana.Service.PaymentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
 public class PaymentController {
+    private final PaymentService paymentService;
 }
---
+++ src/main/java/com/example/travelhana/Service/PaymentService.java
@@ -0,0 +1,5 @@
+package com.example.travelhana.Service;
+
+public class PaymentService {
+
+}
