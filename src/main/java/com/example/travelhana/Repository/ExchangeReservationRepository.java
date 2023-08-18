00410c084a1c2b5b0b5f391e08a5b1557cc3cf44
PARKYUNJU <pyunj6767@gmail.com>
2023-08-18 15:24:42 +0900

    Remove: 요구사항 변화로 불필요한 도메인 삭제

--- src/main/java/com/example/travelhana/Repository/ExchangeReservationRepository.java
+++ src/main/java/com/example/travelhana/Repository/ExchangeReservationRepository.java
@@ -1,6 +1,5 @@
 package com.example.travelhana.Repository;
 
-import com.example.travelhana.Domain.ExchangeReservation;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ExchangeReservationRepository extends JpaRepository<ExchangeReservation, Long> {
