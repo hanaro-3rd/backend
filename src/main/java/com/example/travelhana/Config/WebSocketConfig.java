991f06f517546d3926a469439221997d7f2c4912
PARKYUNJU <pyunj6767@gmail.com>
2023-08-22 14:16:10 +0900

    #49 Feat: 테스트용 controller 추가

--- src/main/java/com/example/travelhana/Config/WebSocketConfig.java
+++ src/main/java/com/example/travelhana/Config/WebSocketConfig.java
@@ -1,15 +1,12 @@
 package com.example.travelhana.Config;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.stereotype.Component;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
-import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
 @Configuration
 @EnableWebSocketMessageBroker
@@ -18,19 +15,16 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
 	@Override
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
-		registry.addEndpoint("/ws-stomp")
-				.withSockJS();
+		registry.addEndpoint("/endpoint")
+				.setAllowedOriginPatterns("*")
+				.withSockJS(); //버전 낮은 브라우저에서도 지원 가능하게
 	}
 
 	@Override
 	public void configureMessageBroker(MessageBrokerRegistry registry) {
-		registry.enableSimpleBroker("/sub");
-		registry.setApplicationDestinationPrefixes("/pub");
-	}
+		registry.enableSimpleBroker("/sub"); //해당 주소를 구독하고 있는 클라이언트들에게 메세지 전달
+		registry.setApplicationDestinationPrefixes("/pub"); //클라이언트에서 보낸 메세지를 받을 prefix
 
-	@Override
-	public void configureClientInboundChannel(ChannelRegistration registration) {
-		registration.interceptors(stompHandler); // 핸들러 등록
 	}
 
 }
