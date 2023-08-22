8d49ecc82088f15e304392f56433f02da168758a
ssangjun <ssangjun4dev@gmail.com>
2023-08-22 20:12:28 +0900

    Test: jenkins 테스트22

--- src/main/java/com/example/travelhana/Service/implement/PhoneAuthServiceImpl.java
+++ src/main/java/com/example/travelhana/Service/implement/PhoneAuthServiceImpl.java
@@ -147,8 +147,7 @@ public class PhoneAuthServiceImpl implements PhoneAuthService {
 
 			if (codeDto.getCode().equals(code)) { //코드가 일치하면
 				Optional<Users> user = userService.validateDuplicateUsername(codeDto.getPhonenum());
-				if (!user.isPresent()) //유저가 존재하지 않으면
-				{
+				if (!user.isPresent()) { //유저가 존재하지 않으면
 					codeResponseDto = CodeResponseDto.builder()
 							.isCodeEqual(true)
 							.isExistUser(false)
