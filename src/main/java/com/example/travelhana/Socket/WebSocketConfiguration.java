e98bfd68db5c7b69f7e8e5ba89eeee0347cdfbbd
PARKYUNJU <pyunj6767@gmail.com>
2023-08-24 16:19:29 +0900

    #49 Feat: 양방향 소켓 연결 위해 메소드 오버라이드

--- src/main/java/com/example/travelhana/Socket/WebSocketConfiguration.java
+++ src/main/java/com/example/travelhana/Socket/WebSocketConfiguration.java
@@ -1,5 +1,6 @@
 package com.example.travelhana.Socket;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
@@ -12,11 +13,11 @@ public class WebSocketConfiguration implements WebSocketConfigurer {
 	@Override
 	public void registerWebSocketHandlers(WebSocketHandlerRegistry registry){
 		registry.addHandler(signalingSocketHandler(),"/sub")
-				.setAllowedOrigins("*")
+				.setAllowedOrigins("*"); //클라이언트에서 웹소켓 서버에 요청 시 모든 요청 수용 (CORS)
 	}
 
-	@Override
+	@Bean
 	public WebSocketHandler signalingSocketHandler(){
-		return new WebSocketHandler()''
+		return new WebSocketHandler();
 	}
 }
