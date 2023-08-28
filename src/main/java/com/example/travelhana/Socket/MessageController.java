b7a45b11a9d544e1f429908d09374e9c9c2876df
PARKYUNJU <pyunj6767@gmail.com>
2023-08-28 14:54:11 +0900

    Feat: 웹소켓 클래스 추가

--- src/main/java/com/example/travelhana/Socket/MessageController.java
+++ src/main/java/com/example/travelhana/Socket/MessageController.java
@@ -1,25 +1,25 @@
-package com.example.travelhana.Socket;
-
-import lombok.RequiredArgsConstructor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.context.event.EventListener;
-import org.springframework.messaging.handler.annotation.Header;
-import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
-import org.springframework.stereotype.Controller;
-import org.springframework.messaging.simp.SimpMessageSendingOperations;
-import org.springframework.web.socket.messaging.SessionConnectEvent;
-import org.springframework.web.socket.messaging.SessionDisconnectEvent;
-
-@Controller
-@RequiredArgsConstructor
-public class MessageController {
-	private final SimpMessageSendingOperations simpMessageSendingOperations;
-
-	@MessageMapping("/alarm")
-	public void message(Message message){
-		simpMessageSendingOperations.convertAndSend("/sub/channel/"+message.getChannelId(),message);
-	}
-
-}
+//package com.example.travelhana.Socket;
+//
+//import lombok.RequiredArgsConstructor;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import org.springframework.context.event.EventListener;
+//import org.springframework.messaging.handler.annotation.Header;
+//import org.springframework.messaging.handler.annotation.MessageMapping;
+//import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.messaging.simp.SimpMessageSendingOperations;
+//import org.springframework.web.socket.messaging.SessionConnectEvent;
+//import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+//
+//@Controller
+//@RequiredArgsConstructor
+//public class MessageController {
+//	private final SimpMessageSendingOperations simpMessageSendingOperations;
+//
+//	@MessageMapping("/alarm")
+//	public void message(Message message){
+//		simpMessageSendingOperations.convertAndSend("/sub/channel/"+message.getChannelId(),message);
+//	}
+//
+//}
--- src/main/java/com/example/travelhana/Socket/WebSocketConfiguration.java
+++ src/main/java/com/example/travelhana/Socket/WebSocketConfiguration.java
@@ -1,34 +1,35 @@
-package com.example.travelhana.Socket;
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.simp.config.ChannelRegistration;
-import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.*;
-
-@Configuration
-@EnableWebSocketMessageBroker
-@RequiredArgsConstructor
-public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer {
-
-	private final SocketPreHandler socketPreHandler;
-	@Override
-	public void registerStompEndpoints(StompEndpointRegistry registry){
-		registry.addEndpoint("/ws")
-				.setAllowedOrigins("*");
-	}
-
-	//클라이언트는 구독 경로 '/sub/channel/{채널아이디}'
-	//서버가 발행할때는 "/pub/alarm"로 메세지 보냄. 메세지 보낼떄는 채널아이디 포함
-	@Override
-	public void configureMessageBroker(MessageBrokerRegistry registry){
-		registry.enableSimpleBroker("/sub");
-		registry.setApplicationDestinationPrefixes("/pub");
-	}
-
-	@Override
-	public void configureClientInboundChannel(ChannelRegistration registration) {
-		registration.interceptors(socketPreHandler);
-	}
-}
+//package com.example.travelhana.Socket;
+//
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.messaging.simp.config.ChannelRegistration;
+//import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+//import org.springframework.web.socket.config.annotation.*;
+//
+//@Configuration
+//@EnableWebSocketMessageBroker
+//@RequiredArgsConstructor
+//public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer {
+//
+////	private final SocketPreHandler socketPreHandler;
+//	@Override
+//	public void registerStompEndpoints(StompEndpointRegistry registry){
+//		registry.addEndpoint("/ws")
+//				.setAllowedOrigins("*");
+//	}
+//
+//	//클라이언트는 구독 경로 '/sub/channel/{채널아이디}'
+//	//서버가 발행할때는 "/pub/alarm"로 메세지 보냄. 메세지 보낼떄는 채널아이디 포함
+//	@Override
+//	public void configureMessageBroker(MessageBrokerRegistry registry){
+//		registry.enableSimpleBroker("/sub");
+//		registry.setApplicationDestinationPrefixes("/pub");
+//	}
+//
+////	@Override
+////	public void configureClientInboundChannel(ChannelRegistration registration) {
+////		registration.interceptors(socketPreHandler);
+////	}
+//
+//}
